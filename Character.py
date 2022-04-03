79f6f4e6df068182ccbfce8c5d22b8f61b02569b
arthur <arthur@arthur.fr>
2022-04-03 22:38:10 +0200

    Update

--- Character.py
+++ Character.py
@@ -1,6 +1,11 @@
 from pprint import pprint
 from random import randrange
 
+XP_FROM_1_TO_2 = 5
+XP_FROM_2_TO_3 = 10
+XP_FROM_3_TO_4 = 20
+XP_FROM_4_TO_5 = 50
+
 
 def strike(text):
     result = ''
@@ -15,6 +20,7 @@ class Character:
         self.is_alive = True
         self.max_hp = 10
         self.hp = 10
+        self.xp = 0
         self.gold = 0
         self.level = 1
         self.left_weapon = None
@@ -45,25 +51,25 @@ class Character:
         elif level_difference == 0:
             return 1
         elif level_difference == -1:
-            return 1.05
+            return 1.2
         elif level_difference == -2:
-            return 1.15
+            return 1.4
         elif level_difference == -3:
-            return 1.25
+            return 1.7
         elif level_difference == -4:
-            return 1.5
-        elif -4 > level_difference > -8:
             return 2
+        elif -4 > level_difference > -8:
+            return 5
         else:
             return 10
 
     def start_fight(self, enemy):
-        print(f"### You meet a {e.name} ! ###")
+        print(f"### You meet a {enemy.name} ! ###")
         print("")
-        print(f"{e.name} is level {e.level}")
-        print(f"{e.name} has {e.hp}")
-        print(f"{e.name} has this weapon in left hand : {e.left_weapon}")
-        print(f"{e.name} has this weapon in right hand : {e.right_weapon}")
+        print(f"{enemy.name} is level {enemy.level}")
+        print(f"{enemy.name} has {e.hp}")
+        print(f"{enemy.name} has this weapon in left hand : {enemy.left_weapon}")
+        print(f"{enemy.name} has this weapon in right hand : {enemy.right_weapon}")
         print("")
         print(f"You are level {self.level}")
         print(f"You have {self.hp}")
@@ -73,7 +79,7 @@ class Character:
 
         run_away = False
         choose = 0
-        while e.is_alive and self.is_alive and not run_away:
+        while enemy.is_alive and self.is_alive and not run_away:
             print("")
             print("### What do you want to do ? ###")
             print("1. Start attacking")
@@ -105,20 +111,25 @@ class Character:
         if damage == 0:
             print("You miss your attack")
         else:
-            print(f"You hit {e.name} for {damage} damage")
+            print(f"You hit {enemy.name} for {damage} damage")
             enemy.hp -= damage
-            print(f"{e.name} is now {e.hp} hp")
-            if e.hp <= 0:
-                e.is_alive = False
-                print(f"### YOU KILL {e.name} !!! ###")
+            print(f"{enemy.name} is now {enemy.hp} hp")
+            if enemy.hp <= 0:
+                enemy.is_alive = False
+                print(f"### YOU KILL {enemy.name} !!! ###")
+                xp_win = coef * randrange(1, 4)
+                print(f"You win {xp_win} xp !")
+                self.get_xp(xp_win)
+                self.get_loot(enemy.id)
                 return
+
         # Enemy turn
-        print(f"{e.name} is attacking you")
+        print(f"{enemy.name} is attacking you")
         damage = randrange(4) * coef
         if damage == 0:
-            print(f"{e.name} miss you")
+            print(f"{enemy.name} miss you")
         else:
-            print(f"{e.name} hit you for {damage} damage")
+            print(f"{enemy.name} hit you for {damage} damage")
             self.hp -= damage
             print(f"You now have {self.hp} hp")
             if self.hp <= 0:
@@ -128,7 +139,7 @@ class Character:
 
         print("")
         print(f"# You have {self.hp} hp remaining #")
-        print(f"# {e.name} is {e.hp} hp remaining #")
+        print(f"# {enemy.name} is {enemy.hp} hp remaining #")
         print("")
 
     def open_inventory(self):
@@ -173,18 +184,53 @@ class Character:
             # Update object in inventory
             self.inventory[1]['hp_potion'] = 0
 
+    def get_xp(self, xp_win):
+        self.xp += xp_win
+
+        if self.level == 1:
+            if self.xp < XP_FROM_1_TO_2:
+                print(f"You need {XP_FROM_1_TO_2 - self.xp} more xp to level up !")
+            else:
+                print("### You level up ! You are now level 2 ! ###")
+                self.xp -= XP_FROM_1_TO_2
+                self.level += 1
+        elif self.level == 2:
+            if self.xp < XP_FROM_2_TO_3:
+                print(f"You need {XP_FROM_2_TO_3 - self.xp} more xp to level up !")
+            else:
+                print("### You level up ! You are now level 3 ! ###")
+                self.xp -= XP_FROM_2_TO_3
+                self.level += 1
+        elif self.level == 3:
+            if self.xp < XP_FROM_3_TO_4:
+                print(f"You need {XP_FROM_3_TO_4 - self.xp} more xp to level up !")
+            else:
+                print("### You level up ! You are now level 4 ! ###")
+                self.xp -= XP_FROM_3_TO_4
+                self.level += 1
+        elif self.level == 4:
+            if self.xp < XP_FROM_4_TO_5:
+                print(f"You need {XP_FROM_4_TO_5 - self.xp} more xp to level up !")
+            else:
+                print("### You level up ! You are now level 5 ! ###")
+                self.xp -= XP_FROM_4_TO_5
+                self.level += 1
+        else:
+            print("###You are already at the maximum level (5) ! Congratulation ! More to come soon ###")
+
+    def get_loot(self, enemy_id):
+        pass
 
 
 class Enemy(Character):
-    def __init__(self, name):
+    def __init__(self, name,enemy_id):
         super().__init__(name)
+        self.id = enemy_id
 
 
 if __name__ == "__main__":
     c = Character("Aanks")
-    e = Enemy("Troll")
+    e = Enemy("Dwarf troll", 0)
 
-    print(f"Character name is {c.name}")
-    print(f"Enemy name is {e.name}")
 
     c.start_fight(e)
